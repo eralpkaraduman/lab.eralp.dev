8b39e96a817ae0b44941093cd101f947d96963e6
Eralp Karaduman <eralp@eralpkaraduman.com>
2020-03-21 03:19:26 +0200

    Added ability to partially render range of lines

--- src/components/source-code.tsx
+++ src/components/source-code.tsx
@@ -1,6 +1,6 @@
 /** @jsx jsx */
 import { jsx } from "theme-ui";
-import { FunctionComponent } from "react";
+import { FunctionComponent, Fragment } from "react";
 import Highlight, { defaultProps, Language } from "prism-react-renderer";
 import theme from "prism-react-renderer/themes/vsDark";
 
@@ -8,13 +8,20 @@ type Props = {
   language: Language;
   code: string;
   lineNumbersEnabled?: boolean;
+  lineRange?: [number, number] | null;
 };
+
+const isLineInRange = (lineNumber: number, [fromLine, toLine]: number[]) =>
+  lineNumber >= fromLine && lineNumber <= toLine;
 
 const SourceCode: FunctionComponent<Props> = ({
   language,
   code,
-  lineNumbersEnabled
+  lineNumbersEnabled,
+  lineRange
 }) => {
+  const shouldRenderLine = (lineNumber: number) =>
+    lineRange ? isLineInRange(lineNumber, lineRange) : true;
   return (
     <div
       css={{
@@ -41,25 +48,29 @@ const SourceCode: FunctionComponent<Props> = ({
             className={className}
             style={style}
           >
-            {tokens.map((line, i) => (
-              <div {...getLineProps({ line, key: i })}>
-                {lineNumbersEnabled && (
-                  <span
-                    css={{
-                      display: "inline-block",
-                      width: "2em",
-                      userSelec: "none",
-                      opacity: 0.3
-                    }}
-                  >
-                    {i + 1}
-                  </span>
-                )}
-                {line.map((token, key) => (
-                  <span {...getTokenProps({ token, key })} />
-                ))}
-              </div>
-            ))}
+            {tokens.map((line, i) =>
+              !shouldRenderLine(i + 1) ? (
+                <Fragment />
+              ) : (
+                <div {...getLineProps({ line, key: i })}>
+                  {lineNumbersEnabled && (
+                    <span
+                      css={{
+                        display: "inline-block",
+                        width: "2em",
+                        userSelec: "none",
+                        opacity: 0.3
+                      }}
+                    >
+                      {i + 1}
+                    </span>
+                  )}
+                  {line.map((token, key) => (
+                    <span {...getTokenProps({ token, key })} />
+                  ))}
+                </div>
+              )
+            )}
           </pre>
         )}
       </Highlight>
@@ -68,7 +79,8 @@ const SourceCode: FunctionComponent<Props> = ({
 };
 
 SourceCode.defaultProps = {
-  lineNumbersEnabled: false
+  lineNumbersEnabled: false,
+  lineRange: null
 } as Partial<Props>;
 
 export default SourceCode;
