0a5590376a7694357581a97a814daddda0efda98
Eralp Karaduman <eralp@eralpkaraduman.com>
2020-03-22 04:22:05 +0200

    Moved font loading upper in tree

--- src/pages/text-3d/text-3d-canvas.tsx
+++ src/pages/text-3d/text-3d-canvas.tsx
@@ -1,43 +1,48 @@
-import React, { Suspense, FunctionComponent } from "react";
+import React, { FunctionComponent } from "react";
 import { Canvas, Dom } from "react-three-fiber";
+import { useLoader } from "react-three-fiber";
 import Text3dObject from "./text-3d-object";
 import { Vector3, Euler, Color } from "three";
+import * as THREE from "three";
 
-const Text3DCanvas: FunctionComponent = () => (
-  <Canvas camera={{ position: [0, 0, 160] }}>
-    <rectAreaLight
-      intensity={4}
-      width={300}
-      height={300}
-      position={[0, 100, 250]}
-    />
-    <Suspense
-      fallback={
-        <Dom center>
-          <div>loading</div>
-        </Dom>
-      }
-    >
+const text1pos = new Vector3(0, 40, 0);
+const text1rot = new Euler(0, 0.4, 0.5);
+const text2pos = new Vector3(0, -40, 0);
+const text2rot = new Euler(-0.1, -0.4, -0.1);
+const fontUrl = "/fonts/bold.blob";
+
+const Text3DCanvas: FunctionComponent = () => {
+  const font = useLoader(THREE.FontLoader, fontUrl);
+  return (
+    <Canvas camera={{ position: [0, 0, 160] }}>
+      <rectAreaLight
+        intensity={4}
+        width={300}
+        height={300}
+        position={[0, 100, 250]}
+      />
       <Text3dObject
+        font={font}
         text={"3D"}
         color={new Color(0x0000ff)}
         size={60}
         height={10}
-        position={new Vector3(0, 40, 0)}
-        rotation={new Euler(0, 0.4, 0.5)}
+        position={text1pos}
+        rotation={text1rot}
         rotationFactor={0.04}
-      ></Text3dObject>
+      />
       <Text3dObject
+        font={font}
         text={"TEXT"}
         color={new Color(0xff0000)}
         size={40}
         height={10}
-        position={new Vector3(0, -40, 0)}
-        rotation={new Euler(-0.1, -0.4, -0.1)}
+        position={text2pos}
+        rotation={text2rot}
         rotationFactor={-0.02}
       />
-    </Suspense>
-  </Canvas>
-);
+    </Canvas>
+  );
+};
 
 export default Text3DCanvas;
--- src/pages/text-3d/text-3d-object.tsx
+++ src/pages/text-3d/text-3d-object.tsx
@@ -1,8 +1,6 @@
 import React, { FunctionComponent, useRef, useMemo } from "react";
-import { useLoader, useUpdate, useFrame } from "react-three-fiber";
+import { useUpdate, useFrame } from "react-three-fiber";
 import * as THREE from "three";
-
-const fontUrl = "/fonts/bold.blob";
 
 type Props = {
   position: THREE.Vector3;
@@ -12,6 +10,7 @@ type Props = {
   size: number;
   height: number;
   color: THREE.Color;
+  font: THREE.Font;
 };
 
 const Text3DCanvas: FunctionComponent<Props> = ({
@@ -21,13 +20,13 @@ const Text3DCanvas: FunctionComponent<Props> = ({
   text,
   size,
   height,
-  color
+  color,
+  font
 }) => {
   const groupRef = useRef<THREE.Group>();
   useFrame(() => {
     groupRef.current!.rotation.y += rotationFactor;
   });
-  const font = useLoader(THREE.FontLoader, fontUrl);
   const config = useMemo<THREE.TextGeometryParameters>(
     () => ({
       font,
@@ -42,17 +41,18 @@ const Text3DCanvas: FunctionComponent<Props> = ({
     }),
     [font, height, size]
   );
-  const mesh = useUpdate<THREE.Mesh>(self => {
-    const size = new THREE.Vector3();
+  const meshRef = useUpdate<THREE.Mesh>(self => {
     self.geometry.computeBoundingBox();
-    self.geometry.boundingBox.getSize(size);
-    self.position.x = -size.x / 2;
-    self.position.y = -size.y / 2;
-    self.position.z = -size.z / 2;
+    const boundingBoxSize = new THREE.Vector3();
+    self.geometry.boundingBox.getSize(boundingBoxSize);
+    // Pivot to center of the bounding box
+    self.position.x = -boundingBoxSize.x / 2;
+    self.position.y = -boundingBoxSize.y / 2;
+    self.position.z = -boundingBoxSize.z / 2;
   }, []);
   return (
     <group ref={groupRef} position={position} rotation={rotation}>
-      <mesh ref={mesh}>
+      <mesh ref={meshRef}>
         <textGeometry attach="geometry" args={[text, config]} />
         <meshStandardMaterial color={color} attach="material" />
       </mesh>
