f6997bfc1a6372a8fe7d86be5f0b4bcb4834363f
Eralp Karaduman <eralp@eralpkaraduman.com>
2020-03-22 05:36:10 +0200

    Moved rotation animation upper in the three

--- src/pages/text-3d/text-3d-canvas.tsx
+++ src/pages/text-3d/text-3d-canvas.tsx
@@ -1,18 +1,51 @@
-import React, { FunctionComponent } from "react";
-import { Canvas, Dom } from "react-three-fiber";
-import { useLoader } from "react-three-fiber";
+import React, { FunctionComponent, createRef, Suspense, Fragment } from "react";
+import { Canvas, useLoader, useFrame } from "react-three-fiber";
 import Text3dObject from "./text-3d-object";
-import { Vector3, Euler, Color } from "three";
 import * as THREE from "three";
+const textColor1 = new THREE.Color(0x0000ff);
+const textColor2 = new THREE.Color(0xff0000);
 
-const text1pos = new Vector3(0, 40, 0);
-const text1rot = new Euler(0, 0.4, 0.5);
-const text2pos = new Vector3(0, -40, 0);
-const text2rot = new Euler(-0.1, -0.4, -0.1);
+const text1pos = new THREE.Vector3(0, 40, 0);
+const text1rot = new THREE.Euler(0, 0.4, 0.5);
+const text2pos = new THREE.Vector3(0, -40, 0);
+const text2rot = new THREE.Euler(-0.1, -0.4, -0.1);
 const fontUrl = "/fonts/bold.blob";
 
-const Text3DCanvas: FunctionComponent = () => {
+const AnimatedTextGroup: FunctionComponent = ({ children }) => {
   const font = useLoader(THREE.FontLoader, fontUrl);
+  const text1Ref = createRef<THREE.Group>();
+  const text2Ref = createRef<THREE.Group>();
+  useFrame(() => {
+    text1Ref.current!.rotation.y += 0.04;
+    text2Ref.current!.rotation.y -= 0.02;
+  });
+  return (
+    <Fragment>
+      <Text3dObject
+        ref={text1Ref}
+        font={font}
+        text={"3D"}
+        color={textColor1}
+        size={60}
+        height={10}
+        position={text1pos}
+        rotation={text1rot}
+      />
+      <Text3dObject
+        ref={text2Ref}
+        font={font}
+        text={"TEXT"}
+        color={textColor2}
+        size={40}
+        height={10}
+        position={text2pos}
+        rotation={text2rot}
+      />
+    </Fragment>
+  );
+};
+
+const Text3DCanvas: FunctionComponent = () => {
   return (
     <Canvas camera={{ position: [0, 0, 160] }}>
       <rectAreaLight
@@ -21,26 +54,9 @@ const Text3DCanvas: FunctionComponent = () => {
         height={300}
         position={[0, 100, 250]}
       />
-      <Text3dObject
-        font={font}
-        text={"3D"}
-        color={new Color(0x0000ff)}
-        size={60}
-        height={10}
-        position={text1pos}
-        rotation={text1rot}
-        rotationFactor={0.04}
-      />
-      <Text3dObject
-        font={font}
-        text={"TEXT"}
-        color={new Color(0xff0000)}
-        size={40}
-        height={10}
-        position={text2pos}
-        rotation={text2rot}
-        rotationFactor={-0.02}
-      />
+      <Suspense fallback={"Loading"}>
+        <AnimatedTextGroup />
+      </Suspense>
     </Canvas>
   );
 };
--- src/pages/text-3d/text-3d-object.tsx
+++ src/pages/text-3d/text-3d-object.tsx
@@ -1,11 +1,10 @@
-import React, { FunctionComponent, useRef, useMemo } from "react";
-import { useUpdate, useFrame } from "react-three-fiber";
+import React, { useMemo, forwardRef } from "react";
+import { useUpdate } from "react-three-fiber";
 import * as THREE from "three";
 
 type Props = {
   position: THREE.Vector3;
   rotation: THREE.Euler;
-  rotationFactor: number;
   text: string;
   size: number;
   height: number;
@@ -13,51 +12,40 @@ type Props = {
   font: THREE.Font;
 };
 
-const Text3DCanvas: FunctionComponent<Props> = ({
-  position,
-  rotation,
-  rotationFactor,
-  text,
-  size,
-  height,
-  color,
-  font
-}) => {
-  const groupRef = useRef<THREE.Group>();
-  useFrame(() => {
-    groupRef.current!.rotation.y += rotationFactor;
-  });
-  const config = useMemo<THREE.TextGeometryParameters>(
-    () => ({
-      font,
-      size,
-      height,
-      curveSegments: 32,
-      bevelEnabled: true,
-      bevelThickness: 3,
-      bevelSize: 1,
-      bevelOffset: 0,
-      bevelSegments: 8
-    }),
-    [font, height, size]
-  );
-  const meshRef = useUpdate<THREE.Mesh>(self => {
-    self.geometry.computeBoundingBox();
-    const boundingBoxSize = new THREE.Vector3();
-    self.geometry.boundingBox.getSize(boundingBoxSize);
-    // Pivot to center of the bounding box
-    self.position.x = -boundingBoxSize.x / 2;
-    self.position.y = -boundingBoxSize.y / 2;
-    self.position.z = -boundingBoxSize.z / 2;
-  }, []);
-  return (
-    <group ref={groupRef} position={position} rotation={rotation}>
-      <mesh ref={meshRef}>
-        <textGeometry attach="geometry" args={[text, config]} />
-        <meshStandardMaterial color={color} attach="material" />
-      </mesh>
-    </group>
-  );
-};
+const Text3DCanvas = forwardRef<THREE.Group, Props>(
+  ({ position, rotation, text, size, height, color, font }, ref) => {
+    const config = useMemo<THREE.TextGeometryParameters>(
+      () => ({
+        font,
+        size,
+        height,
+        curveSegments: 32,
+        bevelEnabled: true,
+        bevelThickness: 3,
+        bevelSize: 1,
+        bevelOffset: 0,
+        bevelSegments: 8
+      }),
+      [font, height, size]
+    );
+    const meshRef = useUpdate<THREE.Mesh>(self => {
+      self.geometry.computeBoundingBox();
+      const boundingBoxSize = new THREE.Vector3();
+      self.geometry.boundingBox.getSize(boundingBoxSize);
+      // Pivot to center of the bounding box
+      self.position.x = -boundingBoxSize.x / 2;
+      self.position.y = -boundingBoxSize.y / 2;
+      self.position.z = -boundingBoxSize.z / 2;
+    }, []);
+    return (
+      <group ref={ref} position={position} rotation={rotation}>
+        <mesh ref={meshRef}>
+          <textGeometry attach="geometry" args={[text, config]} />
+          <meshStandardMaterial color={color} attach="material" />
+        </mesh>
+      </group>
+    );
+  }
+);
 
 export default Text3DCanvas;
