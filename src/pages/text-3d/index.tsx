fbe65380ea7a98b6cd580c4eb76aecbd8f154e10
Eralp Karaduman <eralp@eralpkaraduman.com>
2020-03-22 06:23:08 +0200

    Added clock

--- src/pages/text-3d/index.tsx
+++ src/pages/text-3d/index.tsx
@@ -1,25 +1,54 @@
 /** @jsx jsx */
 import { jsx, Link } from "theme-ui";
-import { FunctionComponent, Fragment } from "react";
+import { FunctionComponent, Fragment, useEffect, useState } from "react";
 import ExperimentFrame from "../../components/experiment-frame";
 import Text3DCanvas from "./text-3d-canvas";
 
-const Text3D: FunctionComponent = () => (
-  <Fragment>
-    <h1>Text 3D</h1>
-    <p>
-      Demo of displaying 3D text and loading custom fonts using WebGL through{" "}
-      <Link href="https://github.com/react-spring/react-three-fiber">
-        react-three-fiber
-      </Link>
-      . Which is a is a <Link href="https://reactjs.org">React</Link> reconciler
-      for <Link href="https://threejs.org">Threejs</Link> on the web and
-      react-native.
-    </p>
-    <ExperimentFrame width={200} height={200}>
-      <Text3DCanvas />
-    </ExperimentFrame>
-  </Fragment>
-);
+const Text3D: FunctionComponent = () => {
+  const [seconds, setSeconds] = useState<string>("00");
+  const [minutes, setMinutes] = useState<string>("00");
+  const [hour, setHour] = useState<string>("00");
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const now = new Date();
+      setSeconds(
+        now
+          .getSeconds()
+          .toString()
+          .padStart(2, "0")
+      );
+      setMinutes(
+        now
+          .getMinutes()
+          .toString()
+          .padStart(2, "0")
+      );
+      setHour(
+        now
+          .getHours()
+          .toString()
+          .padStart(2, "0")
+      );
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+  return (
+    <Fragment>
+      <h1>Text 3D</h1>
+      <p>
+        Demo of displaying 3D text and loading custom fonts using WebGL through{" "}
+        <Link href="https://github.com/react-spring/react-three-fiber">
+          react-three-fiber
+        </Link>
+        . Which is a is a <Link href="https://reactjs.org">React</Link>{" "}
+        reconciler for <Link href="https://threejs.org">Threejs</Link> on the
+        web and react-native.
+      </p>
+      <ExperimentFrame width={300} height={150}>
+        <Text3DCanvas text1={hour} text2={minutes} text3={seconds} />
+      </ExperimentFrame>
+    </Fragment>
+  );
+};
 
 export default Text3D;
--- src/pages/text-3d/text-3d-canvas.tsx
+++ src/pages/text-3d/text-3d-canvas.tsx
@@ -4,50 +4,75 @@ import Text3dObject from "./text-3d-object";
 import * as THREE from "three";
 const textColor1 = new THREE.Color(0x0000ff);
 const textColor2 = new THREE.Color(0xff0000);
+const textColor3 = new THREE.Color(0x00ff00);
+const text1pos = new THREE.Vector3(-100, 0, 0);
+const text2pos = new THREE.Vector3(0, 0, 0);
+const text3pos = new THREE.Vector3(100, 0, 0);
+const text1IinitialRot = new THREE.Euler(0, 0.4, 0.5);
+const text2IinitialRot = new THREE.Euler(-0.1, -0.4, -0.1);
+const text3IinitialRot = new THREE.Euler(0.2, 0.8, 0.1);
 
-const text1pos = new THREE.Vector3(0, 40, 0);
-const text1rot = new THREE.Euler(0, 0.4, 0.5);
-const text2pos = new THREE.Vector3(0, -40, 0);
-const text2rot = new THREE.Euler(-0.1, -0.4, -0.1);
 const fontUrl = "/fonts/bold.blob";
 
-const AnimatedTextGroup: FunctionComponent = ({ children }) => {
+type TextsProps = {
+  text1: string;
+  text2: string;
+  text3: string;
+};
+
+const AnimatedTextGroup: FunctionComponent<TextsProps> = ({
+  text1,
+  text2,
+  text3
+}) => {
   const font = useLoader(THREE.FontLoader, fontUrl);
   const text1Ref = createRef<THREE.Group>();
   const text2Ref = createRef<THREE.Group>();
+  const text3Ref = createRef<THREE.Group>();
   useFrame(() => {
-    text1Ref.current!.rotation.y += 0.04;
-    text2Ref.current!.rotation.y -= 0.02;
+    text1Ref.current!.rotation.x += 0.04;
+    text2Ref.current!.rotation.x -= 0.02;
+    text3Ref.current!.rotation.y += 0.03;
   });
   return (
     <Fragment>
       <Text3dObject
         ref={text1Ref}
         font={font}
-        text={"3D"}
+        text={text1}
         color={textColor1}
-        size={60}
+        size={50}
         height={10}
         position={text1pos}
-        rotation={text1rot}
+        rotation={text1IinitialRot}
       />
       <Text3dObject
         ref={text2Ref}
         font={font}
-        text={"TEXT"}
+        text={text2}
         color={textColor2}
-        size={40}
+        size={50}
         height={10}
         position={text2pos}
-        rotation={text2rot}
+        rotation={text2IinitialRot}
+      />
+      <Text3dObject
+        ref={text3Ref}
+        font={font}
+        text={text3}
+        color={textColor3}
+        size={50}
+        height={10}
+        position={text3pos}
+        rotation={text3IinitialRot}
       />
     </Fragment>
   );
 };
 
-const Text3DCanvas: FunctionComponent = () => {
+const Text3DCanvas: FunctionComponent<TextsProps> = props => {
   return (
-    <Canvas camera={{ position: [0, 0, 160] }}>
+    <Canvas camera={{ position: [0, 0, 120] }}>
       <rectAreaLight
         intensity={4}
         width={300}
@@ -55,7 +80,7 @@ const Text3DCanvas: FunctionComponent = () => {
         position={[0, 100, 250]}
       />
       <Suspense fallback={"Loading"}>
-        <AnimatedTextGroup />
+        <AnimatedTextGroup {...props} />
       </Suspense>
     </Canvas>
   );
